23ad6c397e07040a4d6fb305a8d719593a290d4b
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-04-30 07:34:31 +0300

    End of day 3

--- modules/web/src/com/company/clinic/web/screens/owner/OwnerBrowse.java
+++ modules/web/src/com/company/clinic/web/screens/owner/OwnerBrowse.java
@@ -2,9 +2,11 @@ package com.company.clinic.web.screens.owner;
 
 import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.clinic.entity.Owner;
+import com.haulmont.reports.gui.actions.RunReportAction;
 
 import javax.inject.Inject;
 
@@ -18,6 +20,8 @@ public class OwnerBrowse extends StandardLookup<Owner> {
     private GroupTable<Owner> ownersTable;
     @Inject
     private Notifications notifications;
+    @Inject
+    private Button ownersTableRunReportBtn;
 
     @Subscribe("ownersTable.greet")
     public void onOwnersTableGreet(Action.ActionPerformedEvent event) {
@@ -28,6 +32,11 @@ public class OwnerBrowse extends StandardLookup<Owner> {
                     .withCaption("Hello "+owner.getName())
                     .show();
         }
+    }
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        ownersTableRunReportBtn.setAction(new RunReportAction());
     }
 
 
